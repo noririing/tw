dae9a0cff63f790b7557402ab85dc1775f2f37cd
noririing <noririing@gmail.com>
2015-06-23 16:48:56 +0900

    umm....

--- tw.rb
+++ tw.rb
@@ -12,10 +12,25 @@ client = Twitter::Streaming::Client.new do | config |
   config.access_token_secret        =  twconf["access_token_secret"]
 end
 
-client.sample do |object|
-  if object.is_a?(Twitter::Tweet)
-    if object.lang == "ja" && object.user.lang == "ja"  && !object.retweeted?
-      print #{object.created_at} , #{object.user.name.chomp} , #{object.text.chomp}
-    end
+#client.sample do |object|
+#  if object.is_a?(Twitter::Tweet)
+#    if object.lang == "ja" && object.user.lang == "ja"  && !object.retweeted?
+#      print #{object.created_at} , #{object.user.name.chomp} , #{object.text.chomp}
+#    end
+#  end
+#end
+
+topics = ["スタバ"]
+client.filter(:track => topics.join(",") ) do |status|
+  if status.user.lang == "ja" && !status.text.index("RT")
+    puts "#{status.user.screen_name}: #{status.text}"
   end
 end
+
+#client.filter( :locations => '132.2,29.9,146.2,39.0,138.4,33.5,146.1,46.20' ) do | object |
+#  if object.is_a?(Twitter::Tweet)
+#    if object.lang == "ja" && object.user.lang == "ja"  && !object.retweeted?
+#        print #{object.created_at} , #{object.user.name.chomp} , #{object.text.chomp}
+#    end
+#  end
+#end
